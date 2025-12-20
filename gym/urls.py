17cb49c3961a5c263f25e9ab3fa1f497073437e7
dmytro-malyk-dm <dmytro.malyk7@gmail.com>
2025-12-20 21:51:04 +0200

    Add list and detail view for trainer and workouts and list for schedule

--- gym/urls.py
+++ gym/urls.py
@@ -1,9 +1,22 @@
 from django.urls import path
 
-from gym.views import HomeTemplateView
+from gym.views import (
+    HomeTemplateView,
+    TrainerListView,
+    TrainerDetailView,
+    WorkoutDetailView,
+    WorkoutListView,
+    ScheduleListView
+)
 
 app_name = "gym"
 
 urlpatterns = [
     path("", HomeTemplateView.as_view(), name="index"),
+    path("trainers/", TrainerListView.as_view(), name="trainer-list"),
+    path("trainers/<int:pk>/", TrainerDetailView.as_view(), name="trainer-detail"),
+    path("workouts/<int:pk>/", WorkoutDetailView.as_view(), name="workout-detail"),
+    path("workouts/", WorkoutListView.as_view(), name="workout-list"),
+    path("schedules/", ScheduleListView.as_view(), name="schedule-list"),
+
 ]
--- gym/views.py
+++ gym/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
+from django.views import generic
 from django.views.generic import TemplateView
 
-from gym.models import TrainerProfile, Specialization, ClientProfile
+from gym.models import TrainerProfile, Specialization, ClientProfile, Workout, Schedule
 
 
 class HomeTemplateView(TemplateView):
@@ -16,6 +17,47 @@ class HomeTemplateView(TemplateView):
         context["num_visit"] = num_visit
 
         return context
+
+class TrainerListView(generic.ListView):
+    model = TrainerProfile
+    queryset = TrainerProfile.objects.select_related(
+        "user", "specialization"
+    ).order_by("user__first_name")
+    paginate_by = 5
+
+
+class TrainerDetailView(generic.DetailView):
+    model = TrainerProfile
+    context_object_name = "trainers"
+    queryset = TrainerProfile.objects.select_related(
+        "user", "specialization"
+    ).prefetch_related("workouts")
+
+
+class WorkoutListView(generic.ListView):
+    model = Workout
+    queryset = Workout.objects.select_related(
+        "trainer",
+        "trainer__user"
+    ).order_by("name")
+    paginate_by = 5
+
+
+class WorkoutDetailView(generic.DetailView):
+    model = Workout
+    queryset = Workout.objects.select_related(
+        "trainer", "trainer__user"
+    ).prefetch_related("schedules")
+
+
+class ScheduleListView(generic.ListView):
+    model = Schedule
+    queryset = Schedule.objects.select_related(
+        "workout",
+        "workout__trainer",
+        "workout__trainer__user"
+    )
+
 
 
 
