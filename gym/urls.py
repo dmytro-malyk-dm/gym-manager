bab66520369b870d1b56d92985f0eb914ebfc264
dmytro-malyk-dm <dmytro.malyk7@gmail.com>
2025-12-21 19:41:05 +0200

    feat: add URLs for schedule management

--- gym/urls.py
+++ gym/urls.py
@@ -6,7 +6,8 @@ from gym.views import (
     TrainerDetailView,
     WorkoutDetailView,
     WorkoutListView,
-    ScheduleListView, ScheduleDetailView, ClientRegistrationView, BookingCreateView, BookingCancelView, MyBookingView
+    ScheduleListView, ScheduleDetailView, ClientRegistrationView, BookingCreateView, BookingCancelView, MyBookingView,
+    ScheduleUpdateView, ScheduleDeleteView, ScheduleCreateView
 )
 
 app_name = "gym"
@@ -19,9 +20,24 @@ urlpatterns = [
     path("workouts/", WorkoutListView.as_view(), name="workout-list"),
     path("schedules/", ScheduleListView.as_view(), name="schedule-list"),
     path("schedules/<int:pk>/", ScheduleDetailView.as_view(), name="schedule-detail"),
-    path("register/", ClientRegistrationView.as_view(), name="register"),
     path("schedule/<int:pk>/book/", BookingCreateView.as_view(), name="schedule-book"),
     path("schedule/<int:pk>/cancel/", BookingCancelView.as_view(), name="schedule-cancel"),
-    path("bookings/", MyBookingView.as_view(), name="my-bookings")
+    path(
+        "schedules/create/",
+        ScheduleCreateView.as_view(),
+        name="schedule-create"
+    ),
+    path(
+        "schedules/<int:pk>/update/",
+        ScheduleUpdateView.as_view(),
+        name="schedule-update"
+    ),
+    path(
+        "schedules/<int:pk>/delete/",
+        ScheduleDeleteView.as_view(),
+        name="schedule-delete"
+    ),
+    path("bookings/", MyBookingView.as_view(), name="my-bookings"),
+    path("register/", ClientRegistrationView.as_view(), name="register"),
 
 ]
