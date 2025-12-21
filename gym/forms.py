3a9a8fecbd983c1c804431e3394cacc439418f52
dmytro-malyk-dm <dmytro.malyk7@gmail.com>
2025-12-21 19:37:14 +0200

    feat: add ScheduleForm and ScheduleSearchForm

--- gym/forms.py
+++ gym/forms.py
@@ -2,8 +2,9 @@ import re
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import get_user_model
-from gym.models import ClientProfile
+from django.utils import timezone
 
+from gym.models import ClientProfile, Schedule, Workout
 
 User = get_user_model()
 
@@ -69,3 +70,67 @@ class ClientRegistrationForm(UserCreationForm):
             )
 
         return user
+
+
+class ScheduleForm(forms.ModelForm):
+    """Form for creating/updating schedule"""
+
+    class Meta:
+        model = Schedule
+        fields = ["workout", "start_time", "capacity"]
+        widgets = {
+            'workout': forms.Select(attrs={'class': 'form-control'}),
+            'start_time': forms.DateTimeInput(
+                attrs={
+                    'class': 'form-control',
+                    'type': 'datetime-local'
+                },
+                format='%Y-%m-%dT%H:%M'
+            ),
+            'capacity': forms.NumberInput(attrs={'class': 'form-control'}),
+        }
+
+    def __init__(self, *args, **kwargs):
+        self.user = kwargs.pop('user', None)
+        super().__init__(*args, **kwargs)
+
+        if self.user:
+            if self.user.role == "trainer":
+                self.fields['workout'].queryset = Workout.objects.filter(
+                    trainer__user=self.user
+                )
+            elif self.user.role == "admin":
+                self.fields['workout'].queryset = Workout.objects.all()
+
+    def clean_start_time(self):
+        start_time = self.cleaned_data.get('start_time')
+
+        if start_time and start_time < timezone.now():
+            raise forms.ValidationError(
+                "Cannot create schedule in the past."
+            )
+
+        return start_time
+
+class ScheduleSearchForm(forms.Form):
+    """Form for searching schedule"""
+    date = forms.DateField(
+        required=False,
+        widget=forms.DateInput(
+            attrs={
+                'type': 'date',
+                'class': 'form-control',
+                'placeholder': 'Select date'
+            }
+        )
+    )
+    workout_name = forms.CharField(
+        max_length=255,
+        required=False,
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Search by workout name...'
+            }
+        )
+    )
