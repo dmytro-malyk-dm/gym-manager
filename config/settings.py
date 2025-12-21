51b84116d71c2a0aa91d6b2582bb6855fb31eb07
dmytro-malyk-dm <dmytro.malyk7@gmail.com>
2025-12-21 13:19:17 +0200

    Add authentication configuration and login form

--- config/settings.py
+++ config/settings.py
@@ -124,3 +124,7 @@ STATIC_URL = "static/"
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 
 CRISPY_TEMPLATE_PACK = "bootstrap5"
+
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/"
+LOGIN_URL = "/accounts/login/"
--- config/urls.py
+++ config/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("gym.urls", namespace="gym"))
+    path("", include("gym.urls", namespace="gym")),
+    path("accounts/", include("django.contrib.auth.urls")),
 ]
