52468b6b282411391f13969a1cc4609f01056094
dmytro-malyk-dm <dmytro.malyk7@gmail.com>
2025-12-23 11:09:39 +0200

    Add style and uodate base.html and base.py

--- config/settings/base.py
+++ config/settings/base.py
@@ -93,9 +93,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
 STATIC_URL = "static/"
 
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = "staticfiles/"
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
 
