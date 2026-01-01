ff2a8bb77d86bc4a71c5338c693ff557a62d280d
dmytro-malyk-dm <dmytro.malyk7@gmail.com>
2026-01-01 17:05:09 +0200

    fix: trainer can update own profile without input new password

--- accounts/forms.py
+++ accounts/forms.py
@@ -116,17 +116,27 @@ class TrainerCreationForm(UserCreationForm):
             self.fields["password2"].required = False
             self.fields["password1"].help_text = "Leave blank to keep current password"
 
+            self.fields["username"].required = False
+
             if hasattr(self.instance, "trainer_profile"):
                 self.fields["bio"].initial = self.instance.trainer_profile.bio
                 self.fields["specialization"].initial = (
                     self.instance.trainer_profile.specialization
                 )
 
+    def clean_username(self):
+        username = self.cleaned_data.get("username")
+
+        if self.instance and self.instance.pk:
+            return self.instance.username
+
+        return username
+
     def clean_password2(self):
         password1 = self.cleaned_data.get("password1")
         password2 = self.cleaned_data.get("password2")
 
-        if not password1 and not password2:
+        if self.instance.pk and not password1 and not password2:
             return password2
 
         if password1 and password2 and password1 != password2:
@@ -135,13 +145,15 @@ class TrainerCreationForm(UserCreationForm):
         return password2
 
     def save(self, commit=True):
-        user = super().save(commit=False)
-
-        if not user.pk:
-            user.role = "trainer"
-
-        if user.pk and self.cleaned_data.get("password1"):
-            user.set_password(self.cleaned_data["password1"])
+        if self.instance.pk and not self.cleaned_data.get("password1"):
+            user = self.instance
+            user.email = self.cleaned_data.get("email")
+            user.first_name = self.cleaned_data.get("first_name")
+            user.last_name = self.cleaned_data.get("last_name")
+        else:
+            user = super().save(commit=False)
+            if not user.pk:
+                user.role = "trainer"
 
         if commit:
             user.save()
